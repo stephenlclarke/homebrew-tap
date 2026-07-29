8cc9fe6014f3f54d0e11eb719bab56c8e76f549b
devcontainer release <devcontainer-release@users.noreply.github.com>
2026-07-29 23:17:53 +0100

    chore(devcontainer): update stable formula

--- Formula/devcontainer.rb
+++ Formula/devcontainer.rb
@@ -1,8 +1,8 @@
 class Devcontainer < Formula
   desc "VS Code Dev Containers compatibility for Apple container"
   homepage "https://github.com/stephenlclarke/devcontainer"
-  url "https://github.com/stephenlclarke/devcontainer/releases/download/1.0.0/devcontainer-release-arm64.tar.gz"
-  sha256 "a6dd26b883de6056b12cc65e87ad93ebc1484d6f5cbbfab1dba4aa6e87ea3fdf"
+  url "https://github.com/stephenlclarke/devcontainer/releases/download/1.0.1/devcontainer-release-arm64.tar.gz"
+  sha256 "0cd5210388572fd7c30eeb8f6196f72264c6881b445c13cda73a8c228633d906"
   license "Apache-2.0"
 
   depends_on arch: :arm64
@@ -53,7 +53,7 @@ class Devcontainer < Formula
   end
 
   test do
-    assert_match "1.0.0", shell_output("#{bin}/devcontainer version --short")
+    assert_match "1.0.1", shell_output("#{bin}/devcontainer version --short")
     assert_match "DOCKER_HOST", shell_output("#{bin}/devcontainer context")
     assert_path_exists libexec/"container/plugins/devcontainer/config.toml"
     assert_predicate libexec/"container/plugins/devcontainer/bin/devcontainer", :executable?
